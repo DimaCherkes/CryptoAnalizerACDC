39edbd8a489f200f2636239a6eaf0d2eda3743c4
Dima Cherkes <dmcerkes99@gmail.com>
2024-02-06 02:12:59 +0100

    ADD JUnit tests and Documentation

--- src/main/java/com/javarush/cherkes/ConsoleRunner.java
+++ src/main/java/com/javarush/cherkes/ConsoleRunner.java
@@ -1,20 +1,11 @@
 package com.javarush.cherkes;
 
-import com.javarush.cherkes.actions.Decrypt;
-import com.javarush.cherkes.actions.Encrypt;
-import com.javarush.cherkes.constants.StaticSymbols;
 import com.javarush.cherkes.util.Communication;
 
 public class ConsoleRunner {
     public static void main(String[] args) {
 
-        //Communication.start();
-        Encrypt encryptor = new Encrypt(1);
-        char[] input = "Hello, World!".toCharArray();
-        char[] exp = encryptor.execute(input);
+        Communication.start();
 
-
-        Decrypt decrypt = new Decrypt(1);
-        char[] ecp = decrypt.execute("Ifmmp-!Xpsme\"".toCharArray());
     }
 }
